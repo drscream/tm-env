3d360b3d355c341224888579f08bc25c34fab0fe
Thomas Merkel <tm@core.io>
2020-08-11 23:19:29 +0200

    update spleen font to 1.8.2

---
+++ etc/.fonts/spleen/wsfont/spleen-12x24.c
@@ -0,0 +1,31 @@
+/*
+ * Spleen 1.8.2
+ * Copyright (c) 2018-2020, Frederic Cambus
+ * https://www.cambus.net/
+ *
+ * Created:      2020-06-20
+ * Last Updated: 2020-07-08
+ *
+ * Spleen is released under the BSD 2-Clause license.
+ * See LICENSE file for details.
+ */
+
+#include <stdio.h>
+#include <time.h>
+
+#include <dev/wscons/wsconsio.h>
+#include <dev/wsfont/spleen12x24.h>
+
+int
+main(int argc, char *argv[])
+{
+	size_t loop;
+
+	for (loop = 0; loop < 32 * 2 * 24; loop++)
+		printf("%c", 0);
+
+	for (loop = 0; loop < 224 * 2 * 24; loop++)
+		printf("%c", spleen12x24_data[loop]);
+
+	return 0;
+}
